fad6d1d410fe7cf8bb73810475306401c5a439a1
Hanoodoo7 <mohalmaskati@gmail.com>
2025-07-22 10:34:51 +0300

    ive been pushing in the wrong repo💀

--- controllers/listings.controller.js
+++ controllers/listings.controller.js
@@ -2,6 +2,10 @@ const express = require('express')
 const router = express.Router()
 const isSignedIn = require('../middleware/is-signed-in')
 const Listing = require('../models/listing')
+const upload = require('../config/multer')
+const cloudinary = require('../config/cloudinary')
+
+
 
 // NEW LISTING FORM
 router.get('/new', (req, res) => {
@@ -41,5 +45,79 @@ router.get('/:listingId', async (req, res) => {
     res.redirect('/listings')
   }
 })
+
+// DELETE LISTING FROM DATABASE
+router.delete('/:listingId', isSignedIn, async (req, res) => {
+  const foundListing = await Listing.findById(req.params.listingId).populate('seller')
+
+  if (foundListing.seller._id.equals(req.session.user._id)) {
+    await foundListing.deleteOne()
+    return res.redirect('/listings')
+  }
+
+  return res.send('Not authorized')
+})
+
+// RENDER THE EDIT FORM VIEW
+router.get('/:listingId/edit', isSignedIn, async (req, res) => {
+  const foundListing = await Listing.findById(req.params.listingId).populate('seller')
+
+  if (foundListing.seller._id.equals(req.session.user._id)) {
+    return res.render('listings/edit.ejs', { foundListing: foundListing })
+  }
+
+  return res.send('Not authorized')
+})
+
+// HANDLE EDIT FORM SUBMISSION
+router.put('/:listingId', isSignedIn, async (req, res) => {
+  const foundListing = await Listing.findById(req.params.listingId).populate('seller')
+
+  if (foundListing.seller._id.equals(req.session.user._id)) {
+    await Listing.findByIdAndUpdate(req.params.listingId, req.body, { new: true })
+    return res.redirect(`/listings/${req.params.listingId}`)
+  }
+
+  return res.send('Not authorized')
+})
+
+// POST COMMENT FORM TO THE DATABASE
+router.post('/:listingId/comments', isSignedIn, async (req, res) => {
+const foundListing = await Listing.findById(req.params.listingId)
+  .populate('seller')
+  .populate('comments.author')
+  await foundListing.save()
+  res.redirect(`/listings/${req.params.listingId}`)
+})
+
+router.put('/:listingId/comments/:commentId', isSignedIn, async (req, res) => {
+  const foundListing = await Listing.findById(req.params.listingId)
+  const comment = foundListing.comments.id(req.params.commentId)
+
+  if (comment.author.equals(req.session.user._id)) {
+    comment.content = req.body.content
+    await foundListing.save()
+    res.redirect(`/listings/${req.params.listingId}`)
+  } else {
+    res.send('Not authorized')
+  }
+})
+
+router.delete('/:listingId/comments/:commentId', isSignedIn, async (req, res) => {
+  const foundListing = await Listing.findById(req.params.listingId)
+  const comment = foundListing.comments.id(req.params.commentId)
+
+  if (comment.author.equals(req.session.user._id)) {
+    comment.deleteOne()
+    await foundListing.save()
+    res.redirect(`/listings/${req.params.listingId}`)
+  } else {
+    res.send('Not authorized')
+  }
+})
+
+
+
+
 
 module.exports = router
--- models/listing.js
+++ models/listing.js
@@ -1,15 +1,29 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const listingSchema = new Schema({
+const commentSchema = new mongoose.Schema({
+  content: String,
+  author: { 
+    type: Schema.Types.ObjectId, 
+    ref: 'User' 
+  }
+}, { timestamps: true })
+
+const listingSchema = new mongoose.Schema({
   title: String,
   description: String,
   price: Number,
-  image: String,
-  seller: {
-    type: Schema.Types.ObjectId,
-    ref: 'User'
-  }
+  image: {
+    url: { type: String, required: true },
+    cloudinary_id: { type: String, required: true }
+  },
+  seller: { 
+    type: Schema.Types.ObjectId, 
+    ref: 'User' 
+  },
+  comments: [commentSchema]
 }, { timestamps: true })
+
+
 
 module.exports = mongoose.model('Listing', listingSchema)
