85d074c5262d49d426b9b1126beb7e249e8dbbe6
Hanoodoo7 <mohalmaskati@gmail.com>
2025-07-24 01:28:39 +0300

    finaly works

--- models/listing.js
+++ models/listing.js
@@ -2,28 +2,26 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const commentSchema = new mongoose.Schema({
-  content: String,
-  author: { 
-    type: Schema.Types.ObjectId, 
-    ref: 'User' 
-  }
+    content: String,
+    author: { 
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }
 }, { timestamps: true })
 
 const listingSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  price: Number,
-  image: {
-    url: { type: String, required: true },
-    cloudinary_id: { type: String, required: true }
-  },
-  seller: { 
-    type: Schema.Types.ObjectId, 
-    ref: 'User' 
-  },
-  comments: [commentSchema]
+    title: String,
+    description: String,
+    price: Number, 
+    image: {
+        url: { type: String, required: true },
+        cloudinary_id: { type: String, required: true}
+    },
+    seller: { 
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    comments: [commentSchema]
 }, { timestamps: true })
-
-
 
 module.exports = mongoose.model('Listing', listingSchema)
